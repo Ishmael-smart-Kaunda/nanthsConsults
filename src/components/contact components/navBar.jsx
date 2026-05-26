f83bd0afffba57b00eca06663e64467ec3b4c5ee
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-05-25 22:42:34 -0700

    contacts navbar updated

--- src/components/contact components/navBar.jsx
+++ src/components/contact components/navBar.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 export default function NavBar(){
                         return(
-                                <div className="bg-white w-full flex flex-row justify-between px-5 md:px-25 py-5 shadow-s ">
-                                    <div className="inline-flex items-center gap-10 font-bold">
+                                <div className="fixed top-0 z-10 bg-white w-full flex flex-row justify-between px-5 md:px-25 py-5 shadow-s ">
+                                    <div className="w-full inline-flex justify-between md:justify-start items-center gap-10 font-bold">
                                     <Link to='/' className="inline-flex items-center gap-1 text-[22px] text-ascent font-bold">
                                          Home
                                     </Link>
@@ -14,7 +14,7 @@ export default function NavBar(){
                                          Services
                                     </Link>
                                     </div>
-                                    <div className="inline-flex items-center gap-8 text-ascent">
+                                    <div className="hidden md:inline-flex items-center gap-8 text-ascent">
                                         <a href="" className=""><PiPhoneCallBold className="size-6"/></a>
                                         <a href="" className=""> <PiWhatsappLogoBold className="size-6"/></a>
                                         <a href="" className=""><FiMail className="size-6"/></a> 
